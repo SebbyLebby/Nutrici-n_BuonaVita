1640258862c676250b3a33ec69c0411a1493d413
Sebastián <sebastian23rb@gmail.com>
2022-05-20 14:14:18 -0500

    Eliminando código de más y usando la función construirTd directamente en appendchild de pacienteTr

--- js/form.js
+++ js/form.js
@@ -32,27 +32,13 @@ function construirTr(paciente){
     // Creación de registro nuevo (igual de los elementos nuevos)
     var pacienteTr = document.createElement("tr"); // Obteniendo registro (fila) nuevo
     pacienteTr.classList.add("paciente"); // Agregando clase a cada uno de los elementos creados
-    
-    // Utilizando la función "construirTd" para ahorrar líneas de código
-    var nombreTd = construirTd(paciente.nombre, "info-nombre"); // Elementos que estarán en ese registro
-    var pesoTd = construirTd(paciente.peso, "info-peso");
-    var alturaTd = construirTd(paciente.altura, "info-altura");
-    var gorduraTd = construirTd(paciente.gordura, "info-gordura");
-    var imcTd = construirTd(paciente.imcTd, "info-imc");
-
-    // Cargar los valores en las etiquetas creadas
-    nombreTd.textContent = paciente.nombre; //Agarramos los 4 valores de la clase paciente (nombre, altura, peso, gordura)
-    alturaTd.textContent = paciente.altura;
-    pesoTd.textContent = paciente.peso;
-    gorduraTd.textContent = paciente.gordura;
-    imcTd.textContent = paciente.imc;
 
     // Relacionando valores de los elementos con appendChild hacia el HTML
-    pacienteTr.appendChild(nombreTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    pacienteTr.appendChild(construirTd(paciente.nombre, "info-nombre")); // Utilizando la función "construirTd" para ahorrar líneas de código
+    pacienteTr.appendChild(construirTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(construirTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(construirTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(construirTd(paciente.imc, "info-imc"));
 
     console.log(pacienteTr);
 
