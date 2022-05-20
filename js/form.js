5db0386062c4b822b76b6c05d9f2372683a23925
Sebastián <sebastian23rb@gmail.com>
2022-05-20 13:56:01 -0500

    Creando función para construir nuevo paciente y añadiendo clases para cada elemento construido

--- js/form.js
+++ js/form.js
@@ -5,34 +5,10 @@ botonAdicionar.addEventListener("click",function(event){
     var form = document.querySelector("#form-adicionar")
 
     var paciente = capturarDatosPaciente(form);
-
-    var tabla = document.querySelector("#tabla-pacientes")
-
-    // Creación de registro nuevo (igual de los elementos nuevos)
-    pacienteTr = document.createElement("tr"); //Obteniendo registro (fila) nuevo
-    nombreTd = document.createElement("td"); // Elementos que estarán en ese registro
-    alturaTd = document.createElement("td");
-    pesoTd = document.createElement("td");
-    gorduraTd = document.createElement("td");
-    imcTd = document.createElement("td");
-
-    // Cargar los valores en las etiquetas creadas
-    nombreTd.textContent = paciente.nombre; //Agarramos los 4 valores de la clase paciente (nombre, altura, peso, gordura)
-    alturaTd.textContent = paciente.altura;
-    pesoTd.textContent = paciente.peso;
-    gorduraTd.textContent = paciente.gordura;
-    imcTd.textContent = calcularIMC(peso,altura);
-
-    // Relacionando valores de los elementos con appendChild hacia el HTML
-    pacienteTr.appendChild(nombreTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    var tabla = document.querySelector("#tabla-pacientes");
+    var pacienteTr = construirTr(paciente);
 
     tabla.appendChild(pacienteTr);
-
-    console.log(pacienteTr);
 
 });
 
@@ -44,8 +20,49 @@ function capturarDatosPaciente(form){
         nombre: form.nombre.value,
         peso: form.peso.value,
         altura: form.altura.value,
-        gordura: form.gordura.value
+        gordura: form.gordura.value,
+        imc: calcularIMC(form.peso.value,form.altura.value)
     }
 
     return paciente;
 }
+
+function construirTr(paciente){
+    
+    // Creación de registro nuevo (igual de los elementos nuevos)
+    var pacienteTr = document.createElement("tr"); // Obteniendo registro (fila) nuevo
+    pacienteTr.classList.add("paciente"); // Agregando clase a cada uno de los elementos creados
+    var nombreTd = document.createElement("td"); // Elementos que estarán en ese registro
+    nombreTd.classList.add("info-nombre");
+    var pesoTd = document.createElement("td");
+    pesoTd.classList.add("info-peso");
+    var alturaTd = document.createElement("td");
+    alturaTd.classList.add("info-altura");
+    var gorduraTd = document.createElement("td");
+    gorduraTd.classList.add("info-gordura");
+    var imcTd = document.createElement("td");
+    imcTd.classList.add("info-imc");
+
+    // Cargar los valores en las etiquetas creadas
+    nombreTd.textContent = paciente.nombre; //Agarramos los 4 valores de la clase paciente (nombre, altura, peso, gordura)
+    alturaTd.textContent = paciente.altura;
+    pesoTd.textContent = paciente.peso;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc;
+
+    // Relacionando valores de los elementos con appendChild hacia el HTML
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    console.log(pacienteTr);
+
+    return pacienteTr;
+
+}
+
+/*function construirTd(dato,clase){
+
+}*/
